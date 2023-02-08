ed0a3c4448d0b1d74c1f03d039b9dd8ff3ccb238
mvrk <oscarmenacho23@gmail.com>
2023-02-08 10:40:26 -0500

    trpc setup

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,20 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
 import morgan from 'morgan'
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { createContext, router } from './trpc'
 
 const app = express()
 
+const appRouter = router({
+
+})
+
 app.use(morgan('dev'))
+app.use('/trpc', trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext
+}))
 
 
 export default app
